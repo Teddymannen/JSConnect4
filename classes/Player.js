a1d9edccae70dc3b8582946aebaec8f2281f7636
Teddymannen <teddy_kross@hotmail.com>
2023-08-22 15:50:25 +0200

    Update Player.js

---
+++ classes/Player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
+  }
+}
