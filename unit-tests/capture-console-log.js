2ed4d19360d060b8c7565bc0071a9c70ea99e1ae
Teddymannen <teddy_kross@hotmail.com>
2023-08-29 11:07:27 +0200

    Create capture-console-log.js

---
+++ unit-tests/capture-console-log.js
@@ -0,0 +1,18 @@
+// Monkey patch console.log so we can read and test
+// output to the console
+let orgLogMethod = console.log;
+let savedConsoleLogOutput = [];
+console.log = (...args) => {
+    // Save the console output for tests
+    savedConsoleLogOutput.push(args);
+    // apply takes a function and calls it with 
+    // a certain "this" (in this case the console)
+    // and an array of argument
+    return orgLogMethod.apply(console, args);
+}
+
+module.exports = function getConsoleLogOutput() {
+    let copy = savedConsoleLogOutput.slice();
+    savedConsoleLogOutput.length = 0 // empty original array
+    return copy;
+}
