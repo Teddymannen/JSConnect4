7ccdf53da25171a00ff2227a6dd21d0acc31aec2
Teddymannen <teddy_kross@hotmail.com>
2023-08-29 09:58:04 +0200

    Create test for dropPiece()

--- unit-tests/Board.test.js
+++ unit-tests/Board.test.js
@@ -3,7 +3,6 @@
 require('./load-all-classes.js');
 
 test('Check that a grid is created properly', () => {
-  // Write the logic for our test with Jest expect syntax
   let board = new Board(6, 7);
   expect(board.grid.length).toBe(6);
   for (let i = 0; i < board.grid.length; i++) {
@@ -11,6 +10,14 @@ test('Check that a grid is created properly', () => {
   }
 });
 
-test('Check that a piece can be dropped in an empty column', () => {
-
+test('Check that a piece can only be dropped in an empty column', () => {
+  let board = new Board(6, 7);
+  expect(board.dropPiece(0, 'O')).toBe(true);
+  expect(board.dropPiece(0, 'X')).toBe(true);
+  expect(board.dropPiece(0, 'O')).toBe(true);
+  expect(board.dropPiece(0, 'X')).toBe(true);
+  expect(board.dropPiece(0, 'O')).toBe(true);
+  expect(board.dropPiece(0, 'X')).toBe(true);
+  // Column is full
+  expect(board.dropPiece(0, 'O')).toBe(false);
 });
