6ff76c5c5a5489346904e12a497881fddd8b918c
Teddymannen <teddy_kross@hotmail.com>
2023-08-29 14:07:11 +0200

    Create test for start
    We didn't manage to test prompt, and couldn't progress with this. Might check on it later.

--- unit-tests/Game.test.js
+++ unit-tests/Game.test.js
@@ -1,7 +1,10 @@
 // Game tests here
 // run with "npm test"
 require('./load-all-classes.js');
+const getConsoleLogOutput = require('./capture-console-log.js');
 
-test('dummy test', () => {
-  expect(1).toBe(1);
+test.skip('Check that start() starts the game', () => {
+  // We tried to test prompts but realized it got too complicated for the scope
+  // of this project. 
 });
+
