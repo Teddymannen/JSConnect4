f7047790f92aea71ec0c19965ff2c380628e6f9d
Teddymannen <teddy_kross@hotmail.com>
2023-08-29 15:33:36 +0200

    Create test for makeMove()
    Partly done

--- unit-tests/GameLogic.test.js
+++ unit-tests/GameLogic.test.js
@@ -1,7 +1,61 @@
 // GameLogic tests here
 // run with "npm test"
 require('./load-all-classes.js');
+const getConsoleLogOutput = require('./capture-console-log.js');
 
-test('dummy test', () => {
-  expect(1).toBe(1);
+test('Check that the makeMove() method works.', () => {
+  let game = new Game(6, 7);
+  let board = game.board;
+  let player1 = new Player('Test 1', 'X');
+  let player2 = new Player('Test 2', 'O');
+  let gameLogic = new GameLogic(player1, player2, board);
+  gameLogic.makeMove(0);
+
+  expect(getConsoleLogOutput()).toEqual([
+    ['|   |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['|   |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['|   |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['|   |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['|   |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['| X |   |   |   |   |   |   |'],
+    [' ---------------------------'],
+    ['  1   2   3   4   5   6   7 '],
+    [`It's Test 2's turn.`]
+  ]);
+});
+
+// Skip this test for now. 
+test.skip('Check that the board is full', () => {
+  let game = new Game(6, 7);
+  let board = game.board;
+  let player1 = new Player('Test 1', 'X');
+  let player2 = new Player('Test 2', 'O');
+  let gameLogic = new GameLogic(player1, player2, board);
+  // loop to fill board
+  for (let col = 0; col < board.columns; col++) {
+    for (let i = 0; i < board.rows; i++) {
+      gameLogic.makeMove(col);
+    }
+  }
+  expect(getConsoleLogOutput()).toEqual([
+    ['| O | O | O | O | O | O | O |'],
+    [' ---------------------------'],
+    ['| X | X | X | X | X | X | X |'],
+    [' ---------------------------'],
+    ['| O | O | O | O | O | O | O |'],
+    [' ---------------------------'],
+    ['| X | X | X | X | X | X | X |'],
+    [' ---------------------------'],
+    ['| O | O | O | O | O | O | O |'],
+    [' ---------------------------'],
+    ['| X | X | X | X | X | X | X |'],
+    [' ---------------------------'],
+    ['  1   2   3   4   5   6   7 '],
+    [`It's a draw!`]
+  ]);
 });
