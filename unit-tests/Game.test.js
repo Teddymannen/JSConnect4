310c8ca7dee340cc836b39c39b2638217371d6f7
Teddymannen <teddy_kross@hotmail.com>
2023-09-12 17:34:59 +0200

    Update comment and remove a test

--- unit-tests/Game.test.js
+++ unit-tests/Game.test.js
@@ -1,12 +1,7 @@
 // Game tests here
-// run with "npm test"
+// run with "npm run test-jest"
 require('./load-all-classes.js');
 const getConsoleLogOutput = require('./capture-console-log.js');
-
-test.skip('Check that start() starts the game', () => {
-  // We tried to test prompts but realized it got too complicated for the scope
-  // of this project. 
-});
 
 test('Check that the play() method works', () => {
   let game = new Game(6, 7);
