65014309834ab2019412d360a32187219bc6fb85
Teddymannen <teddy_kross@hotmail.com>
2023-09-12 17:33:48 +0200

    clear up some comments

--- classes/Game.js
+++ classes/Game.js
@@ -11,42 +11,15 @@ class Game {
     this.startRender();
   }
 
-  // start() {
-  //   // clear the board
-  //   this.board = new Board(this.board.rows, this.board.columns);
-  //   console.log('Welcome to Connect Four!');
-  //   // prompt for player names
-  //   // var player1 = prompt('Player 1, what is your name?');
-  //   // var player2 = prompt('Player 2, what is your name?');
-  //   // this.player1 = new Player(player1, 'X');
-  //   // this.player2 = new Player(player2, 'O');
-
-  //   this.gameLogic = new GameLogic(this.player1, this.player2, this.board);
-
-  //   // display the board
-  //   this.board.display();
-  //   // console.log(`Let's begin. ${this.player1.name} goes first.`);
-  //   this.info = `Let's begin. ${this.player1.name} goes first.`;
-  //   // console.log('Play a piece with "game.play(1-7)"');
-  //   this.render(this.info, this.form);
-  //   // Add eventListener to click on columns
-  //   this.addEventListeners();
-  // }
-
   startWithPlayers() {
     // clear the board
     this.board = new Board(this.board.rows, this.board.columns);
     console.log('Welcome to Connect Four!');
-    // this.player1 = new Player(this.player1.name, 'X');
-    // this.player2 = new Player(this.player2.name, 'O');
     this.gameLogic = new GameLogic(this.player1, this.player2, this.board);
-    // prompt('ready?')
 
     // display the board
     this.board.display();
-    // console.log(`Let's begin. ${this.player1.name} goes first.`);
     this.info = `<p>Let's begin. ${this.player1.name} goes first.</p>`;
-    // console.log('Play a piece with "game.play(1-7)"');
     this.render(this.info, this.form);
     this.addEventListeners();
   }
@@ -104,11 +77,9 @@ class Game {
 
   addEventListeners() {
     let tdAll = document.querySelectorAll('td');
-    console.log(tdAll);
     // Add event listener for each column
     tdAll.forEach((td, index) => {
       td.addEventListener('click', () => {
-        console.log('click');
 
         let column = index % 7;
 
@@ -128,10 +99,7 @@ class Game {
             } else {
               cell.classList.add('red');
             }
-            // this.gameLogic.makeMove(column);
             this.play(column + 1);
-            // Update grid with new "tile"
-            // this.board.grid[row][column] = this.gameLogic.currentPlayer.symbol;
             // Stop searching when empty cell is found
             break;
           }
