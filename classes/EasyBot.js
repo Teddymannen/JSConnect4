adddd9e8781636d424998ef3b8afc0251eeed72e
Teddymannen <teddy_kross@hotmail.com>
2023-09-19 13:22:26 +0200

    Create EasyBot.js

---
+++ classes/EasyBot.js
@@ -0,0 +1,21 @@
+class EasyBot {
+    constructor(name, symbol) {
+        this.name = "Easy Bot " + name;
+        this.symbol = symbol;
+    }
+
+    getValidCol(board) {
+        let column = Math.floor(Math.random() * board.columns);
+        while (board.isColumnFull(column)) {
+            column = Math.floor(Math.random() * board.columns);
+        }
+        return column + 1;
+    }
+
+    autoPlay(game) {
+        setTimeout(() => {
+            game.play(this.getValidCol(game.board));
+        }, 1000);
+    }
+
+}
