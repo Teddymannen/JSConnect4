ea1cc03049f7749d5c8902980f6e3a8139fa7f78
Teddymannen <teddy_kross@hotmail.com>
2023-09-26 13:58:29 +0200

    remove redundant code

--- classes/EasyBot.js
+++ classes/EasyBot.js
@@ -12,20 +12,9 @@ class EasyBot {
         return column + 1;
     }
 
-    // autoPlay(game) {
-    //     setTimeout(() => {
-    //         game.play(this.randomMove(game.board));
-    //     }, 2000);
-    // }
-
     autoPlay(game) {
         const timeout = setTimeout(() => {
             game.play(this.randomMove(game.board));
         }, 1000);
-
-        if (game.gameLogic.isGameOver) {
-            console.log("Game over!")
-            clearTimeout(timeout);
-        }
     }
 }
