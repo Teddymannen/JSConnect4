b9c02d638fc0a94bef19cd28a83ffaefc9db832f
Teddymannen <teddy_kross@hotmail.com>
2023-10-09 12:29:06 +0200

    Iframe stuff

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,10 +1,10 @@
-Cypress.Commands.add('getIframeBody', () => {
+Cypress.Commands.add('getIframeBody', (i) => {
   // get the iframe > document > body
   // and retry until the body element is not empty
   cy.log('getIframeBody')
 
   return cy
-    .get('iframe[data-cy="the-frame"]', { log: false })
+    .get(`iframe[data-frame="test${i}"]`, { log: false })
     .its('0.contentDocument.body', { log: false }).should('not.be.empty')
     // wraps "body" DOM element to allow
     // chaining more Cypress commands, like ".find(...)"
